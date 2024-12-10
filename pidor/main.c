033cbf3772c91e036c3eab1054d72045d14f2af1
ostolopov <egor21231bar@gmail.com>
2024-12-11 01:04:49 +0300

    Initial Commit

---
+++ pidor/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pidor
+//
+//  Created by Egor Baranov on 11.12.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
